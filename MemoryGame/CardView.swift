2c85647b2d704840dee8c92a7d7b017c9ba50a2b
Paweł Pańczyk <pawel.panczyk@pollub.edu.pl>
2023-10-13 18:36:13 +0200

    Add symbols

--- MemoryGame/CardView.swift
+++ MemoryGame/CardView.swift
@@ -16,10 +16,11 @@ struct CardView: View {
         ZStack {
             let base = RoundedRectangle(cornerRadius: 12)
             Group{
-                base.fill(.blue)
+                base.fill(.white)
                 base.strokeBorder(lineWidth: 2)
                 Text(content).font(.largeTitle)
-            }.opacity(isFaceUp ? 1 : 0)
+            }
+            .opacity(isFaceUp ? 1 : 0)
             base.fill().opacity(isFaceUp ? 0 : 1)
         }
         .onTapGesture {
--- MemoryGame/ContentView.swift
+++ MemoryGame/ContentView.swift
@@ -8,12 +8,22 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let symbols = ["😁", "😅", "😊", "😍", "🙃", "🙂", "😇", "☺️", "🤣", "😂"]
+    @State var numCards: Int = 4
+    
+//    func adjustCardNumber(by offset: Int, symbol: String) -> some View {
+//        
+//    }
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            ForEach(0..<numCards/2, id: \.self) {index in
+                HStack {
+                    CardView(content: symbols[2*index]).foregroundColor(.blue)
+                    CardView(content: symbols[2*index+1]).foregroundColor(.blue)
+                }.frame(height: 100)
+            }
         }
         .padding()
     }
