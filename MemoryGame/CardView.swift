4e6e119482f9d1ea216a955071d45c3ed902902e
Paweł Pańczyk <pawel.panczyk@pollub.edu.pl>
2023-10-13 17:54:52 +0200

    Update

---
+++ MemoryGame/CardView.swift
@@ -0,0 +1,33 @@
+//
+//  CardView.swift
+//  MemoryGame
+//
+//  Created by student on 11/10/2023.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    
+    let content: String
+    @State var isFaceUp = true
+    
+    var body: some View {
+        ZStack {
+            let base = RoundedRectangle(cornerRadius: 12)
+            Group{
+                base.fill(.blue)
+                base.strokeBorder(lineWidth: 2)
+                Text(content).font(.largeTitle)
+            }.opacity(isFaceUp ? 1 : 0)
+            base.fill().opacity(isFaceUp ? 0 : 1)
+        }
+        .onTapGesture {
+            isFaceUp.toggle()
+        }
+    }
+}
+
+#Preview {
+    CardView(content: "😁")
+}
