450b7472238d9538fde5f463f8b326faeb21ac83
Paweł Pańczyk <pawel.panczyk@pollub.edu.pl>
2023-11-07 15:32:48 +0100

    Create MemoGameModel

---
+++ MemoryGame/MemoGameModel.swift
@@ -0,0 +1,13 @@
+//
+//  MemoGameModel.swift
+//  MemoryGame
+//
+//  Created by student on 07/11/2023.
+//
+
+import Foundation
+
+struct MemoGameModel<CardContent> {
+    private var cards: Array<CardView>
+
+}
